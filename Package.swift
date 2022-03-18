648200ea7151882c4b2e98bad3cb6835930d8fb9
LFrancis <lesliefrancis@alum.mit.edu>
2022-03-18 12:19:16 -0700

    trying to make package.swift work with package manager

--- Package.swift
+++ Package.swift
@@ -1,8 +1,9 @@
 // swift-tools-version:5.0
-
 //
 // The `swift-tools-version` declares the minimum version of Swift required to
 // build this package. Do not remove it.
+
+//         .library(name: "SwiftCharts", type: .dynamic, targets: ["SwiftCharts"])
 
 import PackageDescription
 
@@ -12,13 +13,14 @@ let package = Package(
          .iOS(.v9)
      ],
      products: [
-         .library(name: "SwiftCharts", type: .dynamic, targets: ["SwiftCharts"])
+         .library(name: "SwiftCharts", targets: ["SwiftCharts"])
      ],
      targets: [
         .target(
                name: "SwiftCharts",
-               path: "SwiftCharts/SwiftCharts"
+               path: "SwiftCharts"
         )
      ],
      swiftLanguageVersions: [.v5]
- )
+)
+
